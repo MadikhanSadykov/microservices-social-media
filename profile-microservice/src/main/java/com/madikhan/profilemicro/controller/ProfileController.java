65ff7ee1a1a724f312a33a217a145e324230c213
Madikhan Sadykov <madikhansadykov@gmail.com>
2023-04-22 19:51:51 +0600

    replace interests endpoints

--- profile-microservice/src/main/java/com/madikhan/profilemicro/controller/ProfileController.java
+++ profile-microservice/src/main/java/com/madikhan/profilemicro/controller/ProfileController.java
@@ -2,8 +2,6 @@ package com.madikhan.profilemicro.controller;
 
 import com.madikhan.profilemicro.dto.ProfileDTO;
 import com.madikhan.profilemicro.mapper.ProfileMapper;
-import com.madikhan.profilemicro.model.entity.Interest;
-import com.madikhan.profilemicro.model.entity.Profile;
 import com.madikhan.profilemicro.model.request.ProfileUpdateRequest;
 import com.madikhan.profilemicro.repository.ProfileRepository;
 import com.madikhan.profilemicro.service.impl.ProfileServiceImpl;
@@ -17,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -69,38 +66,6 @@ public class ProfileController {
                 .profilesToDTOList(
                         profileService.listAll()
                 );
-    }
-
-    @PostMapping("/{username}/add/interest")
-    public ResponseEntity<Profile> addInterestToProfile(@PathVariable("username") String username,
-                                                        @RequestBody Interest interest) {
-        Profile profile = profileRepository.findProfileByUsername(username);
-        profile.getInterests().add(interest);
-        return new ResponseEntity<>(profileRepository.save(profile), HttpStatus.OK);
-    }
-
-    @PostMapping("/{username}/add/interest/list")
-    public ResponseEntity<Profile> addInterestListToProfile(@PathVariable("username") String username,
-                                                        @RequestBody List<Interest> interests) {
-        Profile profile = profileRepository.findProfileByUsername(username);
-        profile.getInterests().addAll(interests);
-        return new ResponseEntity<>(profileRepository.save(profile), HttpStatus.OK);
-    }
-
-    @PostMapping("/{username}/remove/interest")
-    public ResponseEntity<Profile> removeInterestFromProfile(@PathVariable("username") String username,
-                                                             @RequestBody Interest interest) {
-        Profile profile = profileRepository.findProfileByUsername(username);
-        profile.getInterests().remove(interest);
-        return new ResponseEntity<>(profileRepository.save(profile), HttpStatus.OK);
-    }
-
-    @PostMapping("/{username}/add/interest/list")
-    public ResponseEntity<Profile> removeInterestListToProfile(@PathVariable("username") String username,
-                                                        @RequestBody List<Interest> interests) {
-        Profile profile = profileRepository.findProfileByUsername(username);
-        profile.getInterests().removeAll(interests);
-        return new ResponseEntity<>(profileRepository.save(profile), HttpStatus.OK);
     }
 
     @GetMapping("/message")
